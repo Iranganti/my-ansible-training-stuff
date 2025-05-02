20a6bda9e270c379ba5ba7f5a14ec1561db4f34b
Srinivasan Iranganti <srinivasan.ivd@gmail.com>
2025-05-02 15:06:41 +0000

    testing

--- all_files/git_push.sh
+++ all_files/git_push.sh
@@ -6,6 +6,7 @@ read -p "Enter commit message: " commit_message
 # Run commands
 cp * ~/mycode/all_files
 cd ~/mycode
+git pull origin main
 git status
 git add /home/student/mycode/*
 git commit -m "$commit_message"
